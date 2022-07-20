dec33b0a7bbb372902471abd8aaf98ae9f83a794
Carlos Balz <carlosmoraes.tm@gmail.com>
2022-07-20 13:30:42 -0300

    Sticker is being generated

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -3,18 +3,27 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 
+import java.awt.Color;
+import java.awt.Font;
+
 public class StickerGenerator {
     public void create() throws Exception{
         BufferedImage originalImage = ImageIO.read(new File("entry/movie.jpg"));
 
         var width = originalImage.getWidth();
         var height = originalImage.getHeight();
-        var newHeight = height + 200;
+        var newHeight = height + 500;
 
         var newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 
         Graphics2D graphics = (Graphics2D) newImage.getGraphics();
-        graphics.drawImage(originalImage, width, height, null);
+        graphics.drawImage(originalImage, 0, 0, null);
+
+        var font = new Font(Font.SANS_SERIF, Font.BOLD, 200);        
+        graphics.setColor(Color.BLACK);
+        graphics.setFont(font);
+
+        graphics.drawString("MY STRING", (width / 5), (newHeight - 250));
 
         ImageIO.write(newImage, "png", new File("output/sticker.png"));
     }
