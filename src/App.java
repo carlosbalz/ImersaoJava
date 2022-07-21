dc7845c8c1caf297c8b4ddb2e7c0a99574581d7e
carlosbalz <carlosmoraes.tm@gmail.com>
2022-07-21 01:20:09 -0300

    stickers being generated for each movie

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -11,11 +12,13 @@ public class App {
         // create a http connection to the public api
         // convert the return json to a list of movies using DTOs
         // then print the extracted data
+        // also will generate WhatsApp stickers from the movies posters links
 
-        String json = returnMoviesListJson();
-        ObjectMapper mapper = new ObjectMapper();
+        var json = returnMoviesListJson();
+        var mapper = new ObjectMapper();
         var items = mapper.readValue(json, Items.class);    
         var movies = items.getItems();
+        var stickerGenerator = new StickerGenerator();
             
         for (MoviesDTO movie : movies) {
             System.out.println("Rank: ".concat(movie.getRank()));            
@@ -23,16 +26,18 @@ public class App {
             System.out.println("Poster: ".concat(movie.getPosterURL()));            
             System.out.println("Rating: ".concat(movie.getRating()));         
             System.out.println("-----//-----");   
+
+            var fileURL = new URL(movie.getPosterURL()).openStream();
+            stickerGenerator.create(fileURL, movie.getTitle() + ".png");
         }
     }
 
     private static String returnMoviesListJson() throws IOException, InterruptedException {
         var url = "https://api.mocki.io/v2/549a5d8b";
         URI address = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();        
-        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+        var client = HttpClient.newHttpClient();        
+        var request = HttpRequest.newBuilder(address).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         return response.body();
     }
 }
- 
--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -1,35 +1,25 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 import javax.imageio.ImageIO;
-
 import java.awt.Color;
 import java.awt.Font;
 
 public class StickerGenerator {
-    public void create() throws Exception{
-        BufferedImage originalImage = ImageIO.read(new File("entry/movie.jpg"));
-
+    public void create(InputStream imageURL, String fileName) throws Exception{
+        BufferedImage originalImage = ImageIO.read(imageURL);
         var width = originalImage.getWidth();
         var height = originalImage.getHeight();
-        var newHeight = height + 500;
+        var newImage = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
+        var graphics = (Graphics2D) newImage.getGraphics();
+        var font = new Font(Font.SANS_SERIF, Font.BOLD, 50);
 
-        var newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
-
-        Graphics2D graphics = (Graphics2D) newImage.getGraphics();
-        graphics.drawImage(originalImage, 0, 0, null);
-
-        var font = new Font(Font.SANS_SERIF, Font.BOLD, 200);        
-        graphics.setColor(Color.BLACK);
+        graphics.drawImage(originalImage, 0, 0, null);                
+        graphics.setColor(Color.GREEN);
         graphics.setFont(font);
+        graphics.drawString("by StickerGenerator", Math.round(width/20), Math.round((height-(height/20))));
 
-        graphics.drawString("MY STRING", (width / 5), (newHeight - 250));
-
-        ImageIO.write(newImage, "png", new File("output/sticker.png"));
-    }
-
-    public static void main(String[] args) throws Exception{
-        var generator = new StickerGenerator();
-        generator.create();
+        ImageIO.write(newImage, "png", new File("output/".concat(fileName)));
     }
 }
